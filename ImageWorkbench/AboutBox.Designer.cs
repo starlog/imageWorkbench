91ab7a2a9f724b1f5ad76a332a1eb117a0c5d781
Starlog <felix.cho.kr@gmail.com>
2015-05-04 14:55:46 +0900

    version 0.6

--- ImageWorkbench/AboutBox.Designer.cs
+++ ImageWorkbench/AboutBox.Designer.cs
@@ -41,10 +41,9 @@
             // 
             this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
             this.pictureBox1.Image = global::ImageWorkbench.Properties.Resources.pooq_logo_png;
-            this.pictureBox1.Location = new System.Drawing.Point(15, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(296, 110);
+            this.pictureBox1.Size = new System.Drawing.Size(259, 88);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -53,36 +52,35 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(94, 146);
+            this.label1.Location = new System.Drawing.Point(82, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 15);
+            this.label1.Size = new System.Drawing.Size(121, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "Image Workbench";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 170);
+            this.label2.Location = new System.Drawing.Point(108, 136);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.Size = new System.Drawing.Size(102, 12);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Version 0.5";
+            this.label2.Text = "Version 0.6(D&&D)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 196);
+            this.label3.Location = new System.Drawing.Point(33, 157);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(276, 15);
+            this.label3.Size = new System.Drawing.Size(219, 12);
             this.label3.TabIndex = 3;
             this.label3.Text = "저작권: 2015년 콘텐츠 연합 플랫폼 (주)";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 232);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(105, 186);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
@@ -90,16 +88,15 @@
             // 
             // AboutBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 289);
+            this.ClientSize = new System.Drawing.Size(284, 231);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AboutBox";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
